9eee15508a55cd0091e7bb099ada55b7a34a1662
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-11 15:11:58 +0000

    muliply, divide complete

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+    // Do the number buttons update the display of the running total?
+    it('should have number buttons that update running total display', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number2')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    })
+
+    // Do the arithmetical operations update the display with the result of the operation?
+
+    // Can multiple operations be chained together?
+
+    // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+    // If a number is divided by zero, is the output 'Not a number'? (You will need to modify the Calculator model to meet this requirement).
+
 });
